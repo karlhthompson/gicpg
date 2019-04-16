78f585b2b197182463cba4cc9f6c9cbc398089ed
Karl <karlht2@illinois.edu>
2019-04-16 15:11:04 -0500

    Modify train

--- generate_train.py
+++ generate_train.py
@@ -12,9 +12,9 @@ from generate_data import decode_adj, get_graph, save_graph_list
 
 # Training function
 def train(args, dataset_train, rnn, output):
-    # check if load existing model
+    # Check whether or not to load an existing model
     if args.load:
-        fname = args.model_save_path + args.fname + 'lstm_' + str(args.load_epoch) + '.dat'
+        fname = args.model_save_path + args.fname + 'gru_' + str(args.load_epoch) + '.dat'
         rnn.load_state_dict(torch.load(fname))
         fname = args.model_save_path + args.fname + 'output_' + str(args.load_epoch) + '.dat'
         output.load_state_dict(torch.load(fname))
@@ -25,57 +25,61 @@ def train(args, dataset_train, rnn, output):
     else:
         epoch = 1
 
-    # initialize optimizer
-    optimizer_rnn = optim.Adam(list(rnn.parameters()), lr=args.lr)
+    # Initialize the optimizer
+    optimizer_mlp = optim.Adam(list(rnn.parameters()), lr=args.lr)
     optimizer_output = optim.Adam(list(output.parameters()), lr=args.lr)
 
-    scheduler_rnn = MultiStepLR(optimizer_rnn, milestones=args.milestones, gamma=args.lr_rate)
+    scheduler_mlp = MultiStepLR(optimizer_mlp, milestones=args.milestones, gamma=args.lr_rate)
     scheduler_output = MultiStepLR(optimizer_output, milestones=args.milestones, gamma=args.lr_rate)
 
-    # start main loop
+    # Start the main loop
     time_all = np.zeros(args.epochs)
-    while epoch<=args.epochs:
+    while epoch <= args.epochs:
         time_start = tm.time()
 
-        # train
+        # Run training
         train_mlp_epoch(epoch, args, rnn, output, dataset_train,
-                        optimizer_rnn, optimizer_output,
-                        scheduler_rnn, scheduler_output)
+                        optimizer_mlp, optimizer_output,
+                        scheduler_mlp, scheduler_output)
 
         time_end = tm.time()
         time_all[epoch - 1] = time_end - time_start
-        # test
-        if epoch % args.epochs_test == 0 and epoch>=args.epochs_test_start:
-            for sample_time in range(1,4):
+
+        # Run testing
+        if epoch % args.epochs_test == 0 and epoch >= args.epochs_test_start:
+            for sample_time in range(1, 4):
                 G_pred = []
-                while len(G_pred)<args.test_total_size:
-                    G_pred_step = test_mlp_epoch(epoch, args, rnn, output, test_batch_size=args.test_batch_size,sample_time=sample_time)
+                while len(G_pred) < args.test_total_size:
+                    G_pred_step = test_mlp_epoch(epoch, args, rnn, output, test_batch_size = args.test_batch_size, 
+                        sample_time = sample_time)
                     G_pred.extend(G_pred_step)
-                # save graphs
+
+                # Save the generated graphs
                 fname = args.graph_save_path + args.fname_pred + str(epoch) +'_'+str(sample_time) + '.dat'
                 save_graph_list(G_pred, fname)
+
             print('test done, graphs saved')
 
 
-        # save model checkpoint
+        # Save model checkpoint
         if args.save:
             if epoch % args.epochs_save == 0:
-                fname = args.model_save_path + args.fname + 'lstm_' + str(epoch) + '.dat'
+                fname = args.model_save_path + args.fname + 'gru_' + str(epoch) + '.dat'
                 torch.save(rnn.state_dict(), fname)
                 fname = args.model_save_path + args.fname + 'output_' + str(epoch) + '.dat'
                 torch.save(output.state_dict(), fname)
         epoch += 1
         
-    np.save(args.timing_save_path+args.fname,time_all)
-
+    np.save(args.timing_save_path + args.fname, time_all)
 
 # Define train function
 def train_mlp_epoch(epoch, args, rnn, output, data_loader,
-                    optimizer_rnn, optimizer_output,
-                    scheduler_rnn, scheduler_output):
+                    optimizer_mlp, optimizer_output,
+                    scheduler_mlp, scheduler_output):
     rnn.train()
     output.train()
     loss_sum = 0
+
     for batch_idx, data in enumerate(data_loader):
         rnn.zero_grad()
         output.zero_grad()
@@ -85,10 +89,11 @@ def train_mlp_epoch(epoch, args, rnn, output, data_loader,
         y_len_max = max(y_len_unsorted)
         x_unsorted = x_unsorted[:, 0:y_len_max, :]
         y_unsorted = y_unsorted[:, 0:y_len_max, :]
-        # initialize lstm hidden state according to batch size
+
+        # Initialize gru hidden state according to batch size
         rnn.hidden = rnn.init_hidden(batch_size=x_unsorted.size(0))
 
-        # sort input
+        # Sort input
         y_len,sort_index = torch.sort(y_len_unsorted,0,descending=True)
         y_len = y_len.numpy().tolist()
         x = torch.index_select(x_unsorted,0,sort_index)
@@ -99,27 +104,30 @@ def train_mlp_epoch(epoch, args, rnn, output, data_loader,
         h = rnn(x, pack=True, input_len=y_len)
         y_pred = output(h)
         y_pred = torch.sigmoid(y_pred)
-        # clean
         y_pred = pack_padded_sequence(y_pred, y_len, batch_first=True)
         y_pred = pad_packed_sequence(y_pred, batch_first=True)[0]
-        # use cross entropy loss
+
+        # Use cross entropy loss
         loss = binary_cross_entropy_weight(y_pred, y)
         loss.backward()
-        # update deterministic and lstm
+
+        # Update deterministic and gru
         optimizer_output.step()
-        optimizer_rnn.step()
+        optimizer_mlp.step()
         scheduler_output.step()
-        scheduler_rnn.step()
+        scheduler_mlp.step()
 
-
-        if batch_idx==0: # only output first batch's statistics
+        # Output only the first batch's statistics for each epoch
+        if batch_idx==0: 
             print('Epoch: {}/{}, train loss: {:.6f}, graph type: {}, num_layer: {}, hidden: {}'.format(
                 epoch, args.epochs,loss.item(), args.graph_type, args.num_layers, args.hidden_size_rnn))
 
-        # logging
+        # Logging
         Logger('loss_'+args.fname, loss.item(), epoch*args.batch_ratio+batch_idx)
 
+        # Update the loss sum
         loss_sum += loss.item()
+
     return loss_sum/(batch_idx+1)
 
 
@@ -143,7 +151,7 @@ def test_mlp_epoch(epoch, args, rnn, output, test_batch_size=16, save_histogram=
         rnn.hidden = Variable(rnn.hidden.data)
     y_pred_long_data = y_pred_long.data.long()
 
-    # save graphs as pickle
+    # Save the generated graphs as a pickle
     G_pred_list = []
     for i in range(test_batch_size):
         adj_pred = decode_adj(y_pred_long_data[i].cpu().numpy())
