6463fef8e99e7002551db0d179f1c1358732a189
Karl <karlht2@illinois.edu>
2019-04-19 16:22:24 -0500

    Update files

--- attribute_main.py
+++ attribute_main.py
@@ -6,17 +6,18 @@ a machine learning classification ensemble, defined in gicpg/attribute_ml.py"""
 # =============================================================================
 # Imports
 # =============================================================================
+import os
 import pickle
 from os import listdir
 from os.path import isfile, join
 from gicpg.attribute_ml import infer_attributes_ml
 
 if __name__ == '__main__':
-
+    
     # Create a list of generated graph files
-    graphfiles = [f for f in listdir("./graphs") if isfile(join("./graphs", f))]
+    graphfiles = [f for f in listdir("./graphs") if isfile(join("./graphs",f))]
 
-    # Input desired epoch and sampling time
+    # Input desired epoch and sampling time of the generated graphs
     epoch = 3000
     sampling = 3
 
@@ -31,8 +32,18 @@ if __name__ == '__main__':
     # infer attributes of all graphs in graphlist
     archname = 'arch_1'
     newgraphlist = []
+    count = 1
     for graph in graphlist:
         graph = infer_attributes_ml(graph, archname, savepred=False)
         newgraphlist.append(graph)
+        print('Progress: graph', count)
+        count += 1
+
+    # Save attributed graphs to file
+    if not os.path.isdir('./attributed_graphs/'):
+        os.makedirs('./attributed_graphs/')
+    with open('attributed_graphs/att_graphs_'+str(epoch)+'_'+str(sampling)+
+            '.pkl', 'wb') as f:
+        pickle.dump(newgraphlist, f)
 
     print()
