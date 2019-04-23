a76dfa28dc1594219533062e35679b43d8de0517
Karl <karlht2@illinois.edu>
2019-04-23 10:57:50 -0500

    Modify files

--- attribute_main.py
+++ attribute_main.py
@@ -19,45 +19,58 @@ if __name__ == '__main__':
     # Create a list of generated graph files
     graphfiles = [f for f in listdir("./graphs") if isfile(join("./graphs",f))]
 
-    # Input desired epoch and sampling time of the generated graphs
-    epoch = 3500
-    sampling = 3
+    # # Input desired epoch and sampling time of the generated graphs
+    # epoch = 3500
+    # sampling = 3
 
-    # Load the generated graphs file  with the desired attributes
-    att = str(epoch) + '_' + str(sampling)
-    for graphfile in graphfiles:
-        if att in graphfile:
-            fname = graphfile
-    with open('./graphs/' + fname, "rb") as f:
-        graphlist = pickle.load(f)
-
-    # Evaluate generated graphs to filter out outliers and identicals
+    # # Load the generated graphs file  with the desired attributes
+    # att = str(epoch) + '_' + str(sampling)
+    # for graphfile in graphfiles:
+    #     if att in graphfile:
+    #         fname = graphfile
+    # with open('./graphs/' + fname, "rb") as f:
+    #     graphlist = pickle.load(f)
+        
+    # Load the original architecture graph
     archname = 'arch_1'
     ArchG = nx.read_graphml('dataset/'+archname+'.graphml')
     ArchGund = max((ArchG.to_undirected().subgraph(c) for c in 
-               nx.connected_components(ArchG.to_undirected())), key=len)
+            nx.connected_components(ArchG.to_undirected())), key=len)
     ArchG = ArchG.subgraph(ArchGund.nodes).copy()
+
+    # Load the generated graph files one by one
     evalgraphlist = []
-    for graph in graphlist:
-        graph = graph.reverse(copy=True)
-        graphund = max((graph.to_undirected().subgraph(c) for c in 
-                   nx.connected_components(graph.to_undirected())), key=len)
-        graph = graph.subgraph(graphund.nodes).copy()
-        if (
-            abs((nx.density(graph)/nx.density(ArchG))-1) >= 0.01 and #density ll
-            abs((nx.density(graph)/nx.density(ArchG))-1) <= 0.20 and #density ul
-            abs((nx.average_clustering(graph)/
-                 nx.average_clustering(ArchG))-1) >= 0.01 and #clustering ll
-            abs((nx.average_clustering(graph)/
-                 nx.average_clustering(ArchG))-1) <= 0.20 and # clustering ul
-            abs((nx.algorithms.local_efficiency(graphund)/
-                 nx.algorithms.local_efficiency(ArchGund))-1) >= 0.01 and #efficiency ll
-            abs((nx.algorithms.local_efficiency(graphund)/
-                 nx.algorithms.local_efficiency(ArchGund))-1) <= 0.20 and #efficiency ul
-            abs((nx.radius(graphund)-nx.radius(ArchGund))) <= 4 and #radius ul
-            abs((nx.diameter(graphund)-nx.diameter(ArchGund))) <= 4): #diameter ul
-                evalgraphlist.append(graph)
-    print('Length of evaluated graphs list is: %i' %(len(evalgraphlist)))
+    fcount = 1
+    for graphfile in graphfiles:
+        with open('./graphs/' + graphfile, "rb") as f:
+            graphlist = pickle.load(f)
+
+        # Evaluate generated graphs to filter out outliers and identicals
+        gcount = 1
+        for graph in graphlist:
+            graph = graph.reverse(copy=True)
+            graphund = max((graph.to_undirected().subgraph(c) for c in 
+                    nx.connected_components(graph.to_undirected())), key=len)
+            graph = graph.subgraph(graphund.nodes).copy()
+            if (abs((nx.density(graph)/nx.density(ArchG))-1) >= 0.01 and
+                abs((nx.density(graph)/nx.density(ArchG))-1) <= 0.25 and
+                abs((nx.average_clustering(graph)/
+                    nx.average_clustering(ArchG))-1) >= 0.01 and
+                abs((nx.average_clustering(graph)/
+                    nx.average_clustering(ArchG))-1) <= 0.25 and 
+                abs((nx.algorithms.local_efficiency(graphund)/
+                    nx.algorithms.local_efficiency(ArchGund))-1) >= 0.01 and 
+                abs((nx.algorithms.local_efficiency(graphund)/
+                    nx.algorithms.local_efficiency(ArchGund))-1) <= 0.25 and 
+                abs((nx.radius(graphund)-nx.radius(ArchGund))) <= 4 and 
+                abs((nx.diameter(graphund)-nx.diameter(ArchGund))) <= 4): 
+                    evalgraphlist.append(graph)
+            print('Evaluation Progress: file %i out of %i: graph %i out of %i' 
+                %(fcount, len(graphfiles), gcount, len(graphlist)))
+            gcount += 1
+        fcount += 1
+    print('Evaluation complete. Length of evaluated graphs list is: %i' 
+        %(len(evalgraphlist)))
 
     # Infer attributes of all evaluated graphs
     archname = 'arch_1'
@@ -73,7 +86,7 @@ if __name__ == '__main__':
     # Save attributed graphs to file
     if not os.path.isdir('./attributed_graphs/'):
         os.makedirs('./attributed_graphs/')
-    filename = 'att_graphs_' + str(epoch) + '_' + str(sampling) + '.pkl'
+    filename = 'att_graphs_' + str(len(graphfiles)) + '_' + str(len(evalgraphlist)) + '.pkl'
     with open('attributed_graphs/' + filename, 'wb') as f:
         pickle.dump(attrgraphlist, f)
-    print('Saved attributed graphs to file: ' + filename)
+    print('Attribution complete. Saved attributed graphs to file: ' + filename)
