787a76fb4094c2bbb2109faa61e52cd750c481ca
Karl <karlht2@illinois.edu>
2019-04-24 14:38:11 -0500

    Update generate_main

--- generate_main.py
+++ generate_main.py
@@ -32,10 +32,14 @@ if __name__ == '__main__':
     graphs_validate = graphs
 
     # Show graphs statistics
-    args.max_num_node = max([len(graphs[i].nodes._nodes._atlas) for i in range(len(graphs))])
-    max_num_edge = max([graphs[i].number_of_edges() for i in range(len(graphs))])
-    min_num_edge = min([graphs[i].number_of_edges() for i in range(len(graphs))])
-    print('total graph num: {}, training set: {}'.format(len(graphs),len(graphs_train)))
+    args.max_num_node = max([len(graphs[i].nodes._nodes._atlas) for i in 
+                        range(len(graphs))])
+    max_num_edge = max([graphs[i].number_of_edges() for i in 
+                   range(len(graphs))])
+    min_num_edge = min([graphs[i].number_of_edges() for i in
+                   range(len(graphs))])
+    print('total graph num: {}, training set: {}'.format(len(graphs),
+                                                         len(graphs_train)))
     print('max number node: {}'.format(args.max_num_node))
     print('max/min number edge: {}; {}'.format(max_num_edge,min_num_edge))
     print('max previous node: {}'.format(args.max_prev_node))
@@ -43,22 +47,29 @@ if __name__ == '__main__':
     # Save ground truth graphs
     save_graph_list(graphs, args.graph_save_path + args.fname_train + '0.dat')
     save_graph_list(graphs, args.graph_save_path + args.fname_test + '0.dat')
-    print('train and test graphs saved at: ', args.graph_save_path + args.fname_test + '0.dat')
+    print('train and test graphs saved at: ', args.graph_save_path + 
+                                              args.fname_test + '0.dat')
 
     # Dataset initialization
-    dataset = Graph_sequence_sampler_pytorch(graphs_train,max_prev_node=args.max_prev_node,max_num_node=args.max_num_node)
+    dataset = Graph_sequence_sampler_pytorch(graphs_train,max_prev_node=
+              args.max_prev_node,max_num_node=args.max_num_node)
 
-    sample_strategy = torch.utils.data.sampler.WeightedRandomSampler([1.0 / len(dataset) for i in range(len(dataset))],
-                        num_samples=args.batch_size*args.batch_ratio, replacement=True)
+    sample_strategy = torch.utils.data.sampler.WeightedRandomSampler(
+                      [1.0/len(dataset) for i in range(len(dataset))],
+                      num_samples=args.batch_size*args.batch_ratio, 
+                      replacement=True)
 
-    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-                        sampler=sample_strategy)
+    dataset_loader = torch.utils.data.DataLoader(dataset, 
+                     batch_size=args.batch_size, num_workers=args.num_workers,
+                     sampler=sample_strategy)
 
     # Model initialization
-    rnn = GRU_plain(input_size=args.max_prev_node, embedding_size=args.embedding_size_rnn, hidden_size=args.hidden_size_rnn, 
+    rnn = GRU_plain(input_size=args.max_prev_node, embedding_size=
+                    args.embedding_size_rnn, hidden_size=args.hidden_size_rnn,
                     num_layers=args.num_layers, has_input=True, has_output=False)
 
-    output = MLP_plain(h_size=args.hidden_size_rnn, embedding_size=args.embedding_size_output, y_size=args.max_prev_node)
+    output = MLP_plain(h_size=args.hidden_size_rnn, embedding_size=
+                       args.embedding_size_output, y_size=args.max_prev_node)
 
     # Start training
     train(args, dataset_loader, rnn, output)
