0ad5073058eccc5e20cb21b1b80b009ad4510c96
Karl <karlht2@illinois.edu>
2019-05-19 10:51:09 -0500

    Update combine_graphs

--- gicpg/utils_combine_graphs.py
+++ gicpg/utils_combine_graphs.py
@@ -6,84 +6,91 @@
 # Imports
 # =============================================================================
 import networkx as nx 
+from os import listdir
 import matplotlib.pyplot as plt 
+from os.path import isfile, join
 from gicpg.utils_check_patterns import check_patterns
 
-def combine_graphs(G1, G2, print_info=False, plot=False):
-    # Use the union function
-    # G3 = nx.disjoint_union(G1, G2)
-
-    # Use the composition function
-    G3 = nx.compose(G1, G2)
+def combine_graphs(archgraph, pattgraphs, print_info=False, plot=False):
+    # Use the networkx composition function
+    allgraphs = [archgraph] + pattgraphs
+    combgraph = nx.compose_all(allgraphs)
 
     if print_info:
         # Display all graphs information
         print()
         print('Architecture graph information:')
-        print(nx.info(G1))
-        if nx.is_directed(G1) == False:
-            print("radius: %d" % nx.radius(G1))
-            print("diameter: %d" % nx.diameter(G1))
-            print("local efficiency: %s" % nx.algorithms.local_efficiency(G1))
-            print("global efficiency: %s" % nx.algorithms.global_efficiency(G1))
-        print("density: %s" % nx.density(G1))
-        print("average clustering: %s" % nx.average_clustering(G1))
+        print(nx.info(archgraph))
+        if nx.is_directed(archgraph) == False:
+            print("radius: %d" % nx.radius(archgraph))
+            print("diameter: %d" % nx.diameter(archgraph))
+            print("local efficiency: %s" % nx.algorithms.local_efficiency(archgraph))
+            print("global efficiency: %s" % nx.algorithms.global_efficiency(archgraph))
+        print("density: %s" % nx.density(archgraph))
+        print("average clustering: %s" % nx.average_clustering(archgraph))
         
         print()
 
-        print('Pattern graph information:')
-        print(nx.info(G2))
-        print()
+        print('Pattern graphs information:')
+        for patt in pattgraphs:
+            print(nx.info(patt))
+            print()
 
         print('Combined graph information:')
-        print(nx.info(G3))
-        if nx.is_directed(G3) == False:
-            if nx.is_connected(G3):
-                print("radius: %d" % nx.radius(G3))
-                print("diameter: %d" % nx.diameter(G3))
-            print("local efficiency: %s" % nx.algorithms.local_efficiency(G3))
-            print("global efficiency: %s" % nx.algorithms.global_efficiency(G3))
-        print("density: %s" % nx.density(G3))
-        print("average clustering: %s" % nx.average_clustering(G3))
+        print(nx.info(combgraph))
+        if nx.is_directed(combgraph) == False:
+            if nx.is_connected(combgraph):
+                print("radius: %d" % nx.radius(combgraph))
+                print("diameter: %d" % nx.diameter(combgraph))
+            print("local efficiency: %s" % nx.algorithms.local_efficiency(combgraph))
+            print("global efficiency: %s" % nx.algorithms.global_efficiency(combgraph))
+        print("density: %s" % nx.density(combgraph))
+        print("average clustering: %s" % nx.average_clustering(combgraph))
         print()
 
         # Check isomorphism
-        isomorph = check_patterns(G3, G2)
-        print("Is the generated and pattern graphs isomorphic: %s" % isomorph)
+        isolist = []
+        for patt in pattgraphs:
+            isomorph = check_patterns(combgraph, patt)
+            isolist.append(isomorph)
+        print("Are the generated and pattern graphs isomorphic: %s" % all(isolist))
         print()
 
         # Check connectivity
-        connected = nx.is_connected(G3.to_undirected())
+        connected = nx.is_connected(combgraph.to_undirected())
         print("Is the generated graph connected: %s" % connected)
         print()
 
     if plot:
-        # Plot the new old and new graphs
+        # Plot the old and new arch graphs
         options = {'line_color': 'grey', 'font_size': 10, 'node_size': 10, 'with_labels': False}
         plt.figure(1)
-        nx.draw(G1, **options)
+        nx.draw(archgraph, **options)
         plt.figure(2)
-        nx.draw(G2, **options)
-        plt.figure(3)
-        nx.draw(G3, **options)
+        nx.draw(combgraph, **options)
         plt.show()
     
-    return G3
+    return combgraph
 
 if __name__ == '__main__':
-    # Input graph 1
+    # Input the arch graph
     archname = 'arch_1'
     G1 = nx.read_graphml('dataset/' + archname + '.graphml')
-    # G1 = G1.to_undirected()
-    # G1 = G1.subgraph(max(nx.connected_component_subgraphs(G1), key=len))
+    G1und = max((G1.to_undirected().subgraph(c) for c in nx.connected_components(G1.to_undirected())), key=len)
+    archgraph = G1.subgraph(G1und.nodes).copy()
+    # archgraph = archgraph.to_undirected()
 
-    # Input graph 2
-    pattname = 'patt_8'
-    G2 = nx.read_graphml('dataset/' + pattname + '.graphml')
-    # G2 = G2.to_undirected()
+    # Input all the pattern graphs
+    pattfiles = [f for f in listdir("./dataset") if isfile(join("./dataset",f)) and 'patt' in f]
+    pattgraphs = []
+    for pattname in pattfiles:
+        G2 = nx.read_graphml('dataset/' + pattname)
+        # G2 = G2.to_undirected()
+        pattgraphs.append(G2)
 
-    # Combine the two graphs
-    G3 = combine_graphs(G1, G2, plot=True)
+    # Combine all the graphs
+    G3 = combine_graphs(archgraph, pattgraphs, print_info=True, plot=True)
+
     # # Save generated graph
     # path = 'dataset/' + archname + '_' + pattname + '.graphml'
     # if isomorph and connected:
