8b6546ce898018c460741278922d99e694ca7822
Karl <karlht2@illinois.edu>
2019-05-25 16:03:14 -0500

    Add pattern files

---
+++ gicpg/pattern_train.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+"""cluster_train - """
+# =============================================================================
+# Imports
+# =============================================================================
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import tensorflow as tf
+import scipy.sparse as sp
+from gicpg.pattern_model import *
+
+
+def embed_graphs(graph, FLAGS):
+    # Load data
+    node_type_s = np.asarray([[v] for k, v in nx.get_node_attributes(graph, 'Type').items()])
+    unique_types = np.unique(node_type_s)
+    le = LabelEncoder().fit(unique_types)
+    node_type = le.transform(node_type_s)
+    node_type = node_type.reshape(-1, 1)
+    enc = OneHotEncoder(handle_unknown='ignore').fit(node_type)
+    node_type = enc.transform(node_type).toarray()
+
+    # Define adjacency and feature matrices
+    adj = nx.adjacency_matrix(graph)
+    features = sp.csr_matrix(node_type)
+
+    # Store original adjacency matrix (without diagonal entries) for later
+    adj_orig = adj
+    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+    adj_orig.eliminate_zeros()
+
+    adj_train = mask_test_edges(adj)
+    adj = adj_train
+
+    # Some preprocessing
+    adj_norm = preprocess_graph(adj)
+
+    # Define placeholders
+    placeholders = {
+        'features': tf.sparse_placeholder(tf.float32),
+        'adj': tf.sparse_placeholder(tf.float32),
+        'adj_orig': tf.sparse_placeholder(tf.float32),
+        'dropout': tf.placeholder_with_default(0., shape=())
+    }
+
+    num_nodes = adj.shape[0]
+
+    features = sparse_to_tuple(features.tocoo())
+    num_features = features[2][1]
+    features_nonzero = features[1].shape[0]
+
+    # Create model
+    model = None
+    if FLAGS.model == 'gcn_ae':
+        model = GCNModelAE(placeholders, num_features, features_nonzero)
+    elif FLAGS.model == 'gcn_vae':
+        model = GCNModelVAE(placeholders, num_features, num_nodes, features_nonzero)
+
+    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
+    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
+
+    # Optimizer
+    with tf.name_scope('optimizer'):
+        if FLAGS.model == 'gcn_ae':
+            opt = OptimizerAE(preds=model.reconstructions,
+                            labels=tf.reshape(tf.sparse_tensor_to_dense(placeholders['adj_orig'],
+                                                                        validate_indices=False), [-1]),
+                            pos_weight=pos_weight,
+                            norm=norm)
+        elif FLAGS.model == 'gcn_vae':
+            opt = OptimizerVAE(preds=model.reconstructions,
+                            labels=tf.reshape(tf.sparse_tensor_to_dense(placeholders['adj_orig'],
+                                                                        validate_indices=False), [-1]),
+                            model=model, num_nodes=num_nodes,
+                            pos_weight=pos_weight,
+                            norm=norm)
+
+    # Initialize session
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    adj_label = adj_train + sp.eye(adj_train.shape[0])
+    adj_label = sparse_to_tuple(adj_label)
+
+    # Train model
+    for epoch in range(FLAGS.epochs):
+
+        # Construct feed dictionary
+        feed_dict = construct_feed_dict(adj_norm, adj_label, features, placeholders)
+        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
+
+        # Run single weight update
+        outs = sess.run([opt.opt_op, opt.cost, opt.accuracy], feed_dict=feed_dict)
+
+        # Get current embedding
+        feed_dict.update({placeholders['dropout']: 0})
+        emb = sess.run(model.z_mean, feed_dict=feed_dict)
+
+    return emb
---
+++ pattern_main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+# Settings
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_integer('epochs', 200, 'Number of epochs to train.')
+flags.DEFINE_integer('hidden1', 32, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 16, 'Number of units in hidden layer 2.')
+flags.DEFINE_float('dropout', 0., 'Dropout rate (1 - keep probability).')
+flags.DEFINE_string('model', 'gcn_vae', 'Model string.')
